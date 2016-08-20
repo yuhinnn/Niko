7908063facccac09dfa1e92002b1838ee5df8019
yuhi_deve <yuhinnn@gmail.com>
2016-08-20 16:47:57 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>一行掲示板</title>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+</head>
+<body bgcolor="FFFFFF" text="#000000">
+<h1>ニコニコ一行掲示板</h1>
+<FORM size="4">テキスト送信のテスト</FORM>
+
+<FORM name="from1" method="post" action="view.php">
+名前:<BR>
+<INPUT type="text" name="onamae">
+<BR>
+<INPUT type="submit" value="送信">
+</body>
+</html>
---
+++ view.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>一行掲示板</title>
+</head>
+<body>
+<?php
+print $_POST['onamae']."さんこんにちは"
+?>
+
+</body>
+</html>
